da733a8c5446f8c8a40b8e6f31dfdc0e4cfc8548
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2024-10-28 21:25:52 +0530

    Create code.sql

---
+++ SQL SELECT/Weather Observation Station 11/code.sql
@@ -0,0 +1,16 @@
+SELECT DISTINCT CITY FROM STATION
+WHERE (
+      CITY NOT LIKE '%a'
+      AND CITY NOT LIKE '%E'
+      AND  CITY NOT LIKE '%i'
+      AND CITY NOT LIKE '%O' 
+      AND  CITY NOT LIKE '%U'
+    ) 
+    OR 
+  (
+    CITY NOT LIKE 'a%' 
+    AND CITY NOT LIKE 'E%' 
+    AND  CITY NOT LIKE 'i%' 
+    AND CITY NOT LIKE 'O%' 
+    AND  CITY NOT LIKE 'U%'
+  );
